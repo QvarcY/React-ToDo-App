a0ca9a05b125553f30a055d528b29f31ec1dbab7
Ingars <info@kas.id.lv>
2023-03-23 23:28:10 +0200

    Added task edit, save, star important functions, moved adding function inside to tasks block in App.js, and added some dark style to App.css

--- src/App.js
+++ src/App.js
@@ -2,25 +2,69 @@ import "./App.css";
 import { useState } from "react";
 
 const ToDo = (props) => {
-  const [completed, setCompleted] = useState(false);
-  const [important, setImportant] = useState(false);
+
+  const checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('click', function() {
+    if (this.checked) {
+      this.classList.add('checked');
+    } else {
+      this.classList.remove('checked');
+    }
+  });
+
+  const [completed, setCompleted] = useState(props.completed);
+  const [important, setImportant] = useState(props.important);
+  const [description, setDescription] = useState(props.description);
+  const [startTime, setStartTime] = useState(props.startTime);
+  const [endTime, setEndTime] = useState(props.endTime);
+
+  const handleUpdate = () => {
+    props.onUpdate({
+      id: props.id,
+      completed,
+      important,
+      description,
+      startTime,
+      endTime
+    });
+  };
 
   return (
     <tr>
       <td>
-        <label>{props.description}</label>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </td>
       <td>
-        <input type="checkbox" onChange={() => setCompleted(!completed)} />
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={() => setCompleted(!completed)}
+        />
       </td>
       <td onClick={() => setImportant(!important)}>
         {important ? "⭐️" : ""}
       </td>
-      <td className={completed ? "completed" : ""}>
-        <label>{props.startTime} - {props.endTime}</label>
+      <td>
+        <input
+          type="time"
+          value={startTime}
+          onChange={(e) => setStartTime(e.target.value)}
+        />
+        -
+        <input
+          type="time"
+          value={endTime}
+          onChange={(e) => setEndTime(e.target.value)}
+        />
       </td>
       <td>
-        <button onClick={props.onDelete}>Dzēst</button>
+        <button onClick={handleUpdate}>Saglabāt/Save</button>
+        <button onClick={props.onDelete}>Dzēst/Delete</button>
       </td>
     </tr>
   );
@@ -31,12 +75,16 @@ function App() {
     {
       id: "todo1",
       description: "Pirkt pienu",
+      completed: false,
+      important: false,
       startTime: "18:00",
       endTime: "18:30"
     },
     {
       id: "todo2",
       description: "Iziet ar suni",
+      completed: false,
+      important: false,
       startTime: "20:30",
       endTime: "21:00"
     },
@@ -56,6 +104,8 @@ function App() {
     const newTask = {
       id: `todo${todos.length + 1}`,
       description: newTaskDescription,
+      completed: false,
+      important: false,
       startTime: newTaskStartTime,
       endTime: newTaskEndTime
     };
@@ -65,47 +115,78 @@ function App() {
     setNewTaskEndTime("");
   };
 
-  const handleDeleteTask = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
+  const handleUpdateTask = (updatedTask) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === updatedTask.id ? updatedTask : todo
+    );
+    setTodos(updatedTodos);
   };
 
+  const handleDeleteTask = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+  
   return (
-    <div>
-      <div>
-        Uzdevuma apraksts:
-        <input
-          type="text"
-          value={newTaskDescription}
-          onChange={(e) => setNewTaskDescription(e.target.value)}
-        />
-        Sākuma laiks:
-        <input
-          type="time"
-          value={newTaskStartTime}
-          onChange={(e) => setNewTaskStartTime(e.target.value)}
-        />
-        Beigu laiks:
-        <input
-          type="time"
-          value={newTaskEndTime}
-          onChange={(e) => setNewTaskEndTime(e.target.value)}
-        />
-        <button onClick={handleAddTask}>Pievienot jaunu uzdevumu</button>
-      </div>
-
-      <button onClick={reverseOrder}>Pārkārtot</button>
-
-      <table>
-        <tbody>
-          {todos.map((todo, index) => (
-            <ToDo key={index} {...todo} onDelete={() => handleDeleteTask(index)} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+  <div className="App">
+  <h1>ToDo List - Uzdevumu saraksts</h1>
+  <table>
+  <thead>
+  <tr>
+  <th>Uzdevums/Task</th>
+  <th>Pabeigts/Finished</th>
+  <th>Svarīgs/Important</th>
+  <th>Laiks/Time</th>
+  <th>Darbības/Options</th>
+  </tr>
+  </thead>
+  <tbody>
+  {todos.map((todo) => (
+  <ToDo
+  key={todo.id}
+  id={todo.id}
+  completed={todo.completed}
+  important={todo.important}
+  description={todo.description}
+  startTime={todo.startTime}
+  endTime={todo.endTime}
+  onUpdate={handleUpdateTask}
+  onDelete={() => handleDeleteTask(todo.id)}
+  />
+  ))}
+  <tr>
+  <td>
+  <input
+  type="text"
+  placeholder="Apraksts"
+  value={newTaskDescription}
+  onChange={(e) => setNewTaskDescription(e.target.value)}
+  />
+  </td>
+  <td></td>
+  <td></td>
+  <td>
+  <input
+  type="time"
+  value={newTaskStartTime}
+  onChange={(e) => setNewTaskStartTime(e.target.value)}
+  />
+  -
+  <input
+  type="time"
+  value={newTaskEndTime}
+  onChange={(e) => setNewTaskEndTime(e.target.value)}
+  />
+  </td>
+  <td>
+  <button onClick={handleAddTask}>Pievienot/Add</button>
+  </td>
+  </tr>
+  </tbody>
+  </table>
+  <button onClick={reverseOrder}>Apgriezt secību/Reorder</button>
+  </div>
   );
-}
-
-export default App;
+  }
+  
+  export default App;
